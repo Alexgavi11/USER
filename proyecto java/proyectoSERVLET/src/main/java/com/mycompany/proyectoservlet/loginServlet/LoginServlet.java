4bc765b446764ed688e9681a838c6814a97683e7
Alexgavi11 <157065654+Alexgavi11@users.noreply.github.com>
2024-11-13 23:53:10 -0500

    login

---
+++ proyecto java/proyectoSERVLET/src/main/java/com/mycompany/proyectoservlet/loginServlet/LoginServlet.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.mycompany.proyectoservlet.loginServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author USER
+ */
+@WebServlet(name = "Login", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String usuario=request.getParameter("usuario");
+        String password=request.getParameter("password");
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, e);
+        }
+        String url="jdbc:mysql://localhost:3306/ejercicio";
+        Connection conexion;
+        Statement statement;
+        ResultSet Rs;
+        
+        try {
+           conexion=DriverManager.getConnection(url,"root","");
+           statement=conexion.createStatement();
+           
+           Rs=statement.executeQuery("SELECT * FROM `usuario` where usuario='"+usuario+"' AND password='"+password+"'");
+        
+           if(Rs.next()){
+           request.getSession().setAttribute("usuario",usuario);
+           response.sendRedirect("panel.jsp");
+           } else {
+               response.sendRedirect("index.html");
+           }
+        } catch (Exception e) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ proyecto java/proyectoSERVLET/src/main/webapp/panel.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : panel
+    Created on : 25/08/2024, 12:59:36 a. m.
+    Author     : USER
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
+        <title>turiscaribbean</title>
+    </head>
+    <body>
+         <h1>Bienvenido a turiscaribbean</h1>
+    </body>
+</html>
